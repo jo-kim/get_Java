1b4a9146b9c27a2188b80966c90a84398e94da17
jo-kim <belongs2jo@gmail.com>
2022-07-09 23:02:52 +0900

    return & call stack

--- src/ch04/Ex04_1.java
+++ src/ch04/Ex04_1.java
@@ -9,6 +9,7 @@ public class Ex04_1 {
         long result2 = mm.substract(5L , 3L);
         long result3 = mm.multiply(5L, 3L);
         double result4 = mm.divide(5L, 3L);
+        mm.printGugudan(3);
 
         System.out.println("max(5L, 3L) = " +result);
         System.out.println("add(5L, 3L) = " +result1);
@@ -21,6 +22,15 @@ public class Ex04_1 {
 
 class MyMath {
 
+    void printGugudan(int dan){
+        if(!(2<=dan && dan <=9)){
+            return; // 입력받은 단이 2~9 가 아니면 ,메서드 종료하고 돌아가기
+        }
+        for(int i=1; i<=9; i++){
+            System.out.printf("%d * %d = %d%n",dan, i ,dan * i);
+        }
+        return;
+    }
     long add(long a, long b){
 
         return a + b ;
---
+++ src/ch04/Ex04_2.java
@@ -0,0 +1,7 @@
+package ch04;
+
+public class Ex04_2 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
